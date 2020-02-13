e44d1215e60296a9fba6f9356668f6b4e539bb0c
amirisback <faisalamircs@gmail.com>
2020-02-13 22:32:12 +0700

    add: base project admob

--- app/src/main/java/com/frogobox/basewebview/MainActivity.kt
+++ app/src/main/java/com/frogobox/basewebview/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.frogobox.basewebview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.frogobox.basewebview.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
